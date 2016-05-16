87f04f47003998dd01c72c1d42d8fd0400caf351
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2016-05-16 19:46:27 +0200

    Preserve carret position after receiving content

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,10 +1,26 @@
 import {Socket} from "phoenix" ;
 
+function setSelectionRange(input, selectionStart, selectionEnd) {
+  if (input.setSelectionRange) {
+    input.focus();
+    input.setSelectionRange(selectionStart, selectionEnd);
+  }
+  else if (input.createTextRange) {
+    var range = input.createTextRange();
+    range.collapse(true);
+    range.moveEnd('character', selectionEnd);
+    range.moveStart('character', selectionStart);
+    range.select();
+  }
+}
+
 class App {
   static init() {
     let socket = new Socket("/socket")
     socket.connect()
     socket.onClose( e => console.log("Closed connection") )
+
+    let carretPosition = null;
 
     var channel = socket.channel("docs:test", {})
     channel.join()
@@ -13,6 +29,8 @@ class App {
 
     $('#text').on('keyup', e => {
       if ((e.keyCode >= 32 && e.keyCode <= 126) || e.keyCode == 13) {
+        carretPosition = $('#text')[0].selectionStart;
+
         channel.push("new:content", {
           content: $('#text').val(),
         })
@@ -21,6 +39,8 @@ class App {
 
     channel.on( "new:content", msg => {
       $('#text').val(msg.content);
+
+      setSelectionRange($('#text')[0], carretPosition, carretPosition);
     })
   }
 }
