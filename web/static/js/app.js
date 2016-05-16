19a5c2610570bcdb389161d68c7da29156eeadc8
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2016-05-16 19:50:12 +0200

    Extract setSelectionRange function to separate file

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,18 +1,6 @@
-import {Socket} from "phoenix" ;
+import { Socket } from "phoenix" ;
 
-function setSelectionRange(input, selectionStart, selectionEnd) {
-  if (input.setSelectionRange) {
-    input.focus();
-    input.setSelectionRange(selectionStart, selectionEnd);
-  }
-  else if (input.createTextRange) {
-    var range = input.createTextRange();
-    range.collapse(true);
-    range.moveEnd('character', selectionEnd);
-    range.moveStart('character', selectionStart);
-    range.select();
-  }
-}
+import { setSelectionRange } from './setSelectionRange';
 
 class App {
   static init() {
