4b86ec440ad562fea8205c8ec9625d55eaa0938e
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2016-05-19 21:02:50 +0200

    Add comeonin

--- mix.exs
+++ mix.exs
@@ -36,6 +36,7 @@ defmodule Docs.Mixfile do
      {:phoenix_html, "~> 2.4"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
+     {:comeonin, "~> 1.0"},
      {:cowboy, "~> 1.0"}]
   end
 
