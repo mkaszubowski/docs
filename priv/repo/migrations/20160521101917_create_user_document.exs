342fb256e08ee76e688619c0d9c475cc1e78f63b
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2016-05-26 22:49:05 +0200

    Add users_documents migration

--- priv/repo/migrations/20160521101917_create_user_document.exs
+++ priv/repo/migrations/20160521101917_create_user_document.exs
@@ -2,5 +2,16 @@ defmodule Docs.Repo.Migrations.CreateUserDocument do
   use Ecto.Migration
 
   def change do
+    create table(:users_documents) do
       add :user_id, references(:users, on_delete: :nothing)
       add :document_id, references(:documents, on_delete: :nothing)
+      add :type, :string
+
+      timestamps
+    end
+
+    create index(:users_documents, [:user_id])
+    create index(:users_documents, [:document_id])
+    create unique_index(:users_documents, [:user_id, :document_id])
+  end
+end
