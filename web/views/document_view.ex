167a9858943fc152dacc4ecf24176d2f88101c19
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2016-06-04 00:19:22 +0200

    Display invitations link only for document's owner

--- web/views/document_view.ex
+++ web/views/document_view.ex
@@ -1,3 +1,10 @@
 defmodule Docs.DocumentView do
   use Docs.Web, :view
+
+  def invitations_link(conn, document) do
+    if document.owner_id == conn.assigns.current_user.id do
+      link("View invitations",
+        to: document_invitation_path(conn, :index, document.id))
+    end
+  end
 end
